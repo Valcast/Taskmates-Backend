0702af0897fd96cf465d24ba5a7b5239d4b38bb8
kacpe <kacper.pelka03@gmail.com>
2024-12-01 03:03:11 +0100

    Spring App with Hibernate Init, Docker Init

---
+++ src/test/java/com/drivespace/taskmatesbackend/TaskmatesBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.drivespace.taskmatesbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskmatesBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
