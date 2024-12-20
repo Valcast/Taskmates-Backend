26ed4f02b6f3c0c03a035c447f32daddfd23417b
Kacper <kacper.pelka03@gmail.com>
2024-12-20 02:29:27 +0100

    Refactor db schema

--- db/migrations/backend_db/V1__init_schema.sql
+++ db/migrations/backend_db/V1__init_schema.sql
@@ -17,7 +17,6 @@ CREATE TABLE Users
     updated_at          TIMESTAMP        DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
 
-CREATE TYPE project_status AS ENUM ('ACTIVE', 'COMPLETED', 'CANCELLED');
 
 CREATE TABLE Projects
 (
@@ -25,47 +24,33 @@ CREATE TABLE Projects
     name        VARCHAR(255)                               NOT NULL,
     description TEXT,
     owner_id    UUID                                       NOT NULL REFERENCES users (id) ON DELETE CASCADE,
-    status      project_status                             NOT NULL DEFAULT 'ACTIVE',
+    status      VARCHAR(20)                                NOT NULL DEFAULT 'ACTIVE',
     deadline    TIMESTAMP,
     created_at  TIMESTAMP        DEFAULT CURRENT_TIMESTAMP NOT NULL,
     updated_at  TIMESTAMP        DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
 
-CREATE TYPE member_role AS ENUM ('OWNER', 'ADMIN', 'MEMBER');
 
 CREATE TABLE UsersProjects
 (
     user_id    UUID        NOT NULL REFERENCES Users (id) ON DELETE CASCADE,
     project_id UUID        NOT NULL REFERENCES Projects (id) ON DELETE CASCADE,
-    role       member_role NOT NULL DEFAULT 'MEMBER',
+    role       VARCHAR(20) NOT NULL DEFAULT 'MEMBER',
     added_at   TIMESTAMP            DEFAULT CURRENT_TIMESTAMP NOT NULL,
     PRIMARY KEY (user_id, project_id)
 );
 
-CREATE TYPE task_status AS ENUM ('PENDING', 'IN_PROGRESS', 'COMPLETED', 'CANCELLED');
-CREATE TYPE task_priority AS ENUM ('LOW', 'MEDIUM', 'HIGH', 'URGENT');
-
 CREATE TABLE Tasks
 (
-    id          UUID PRIMARY KEY       DEFAULT gen_random_uuid(),
-    title       VARCHAR(255)  NOT NULL,
+    id          UUID PRIMARY KEY      DEFAULT gen_random_uuid(),
+    title       VARCHAR(255) NOT NULL,
     description TEXT,
-    project_id  UUID          NOT NULL REFERENCES Projects (id) ON DELETE CASCADE,
-    status      task_status   NOT NULL DEFAULT 'PENDING',
-    priority    task_priority NOT NULL DEFAULT 'MEDIUM',
+    project_id  UUID         NOT NULL REFERENCES Projects (id) ON DELETE CASCADE,
+    status      VARCHAR(20)  NOT NULL DEFAULT 'PENDING',
+    priority    VARCHAR(20)  NOT NULL DEFAULT 'MEDIUM',
     deadline    TIMESTAMP,
-    created_at  TIMESTAMP              DEFAULT CURRENT_TIMESTAMP NOT NULL,
-    updated_at  TIMESTAMP              DEFAULT CURRENT_TIMESTAMP NOT NULL
-);
-
-CREATE TABLE TasksHistory
-(
-    id         UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    task_id    UUID                                       NOT NULL REFERENCES Tasks (id) ON DELETE CASCADE,
-    status     VARCHAR(20)                                NOT NULL,
-    priority   VARCHAR(10)                                NOT NULL,
-    updated_at TIMESTAMP        DEFAULT CURRENT_TIMESTAMP NOT NULL,
-    updated_by UUID                                       NOT NULL REFERENCES users (id)
+    created_at  TIMESTAMP             DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    updated_at  TIMESTAMP             DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
 
 CREATE TABLE TasksUsers
@@ -84,25 +69,12 @@ CREATE TABLE Subtasks
     task_id      UUID NOT NULL REFERENCES Tasks (id) ON DELETE CASCADE
 );
 
-CREATE TABLE Comments
-(
-    id         UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    content    TEXT                                       NOT NULL,
-    user_id    UUID                                       NOT NULL REFERENCES Users (id) ON DELETE CASCADE,
-    task_id    UUID REFERENCES Tasks (id) ON DELETE CASCADE,
-    subtask_id UUID REFERENCES Subtasks (id) ON DELETE CASCADE,
-    project_id UUID REFERENCES Projects (id) ON DELETE CASCADE,
-    created_at TIMESTAMP        DEFAULT CURRENT_TIMESTAMP NOT NULL
-);
-
-CREATE TYPE invitation_status AS ENUM ('PENDING', 'ACCEPTED', 'REJECTED');
-
-CREATE TABLE Invitation
+CREATE TABLE Invitations
 (
     id         UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     project_id UUID                                       NOT NULL REFERENCES Projects (id),
     user_id    UUID                                       NOT NULL REFERENCES Users (id),
-    status     invitation_status                          NOT NULL DEFAULT 'PENDING',
+    status     VARCHAR(20)                                NOT NULL DEFAULT 'PENDING',
     created_at TIMESTAMP        DEFAULT CURRENT_TIMESTAMP NOT NULL,
     updated_at TIMESTAMP        DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
--- db/migrations/backend_db/V1_sample_data.sql
+++ db/migrations/backend_db/V1_sample_data.sql
@@ -32,11 +32,6 @@ VALUES ('e9f9bd6d-bf44-42ae-823f-c73f7fcf1b3e', 'Design Homepage', 'Design the h
         'd3012498-9fe3-47b1-98d4-95f39a66d9ff', 'IN_PROGRESS', 'MEDIUM', '2025-02-01', '2024-12-05 09:00:00',
         '2024-12-05 09:00:00');
 
-INSERT INTO TasksHistory (task_id, status, priority, updated_at, updated_by)
-VALUES ('e9f9bd6d-bf44-42ae-823f-c73f7fcf1b3e', 'PENDING', 'HIGH', '2024-12-01 09:00:00',
-        'fef2db0c-2c2e-4f97-b639-7c97b8f8291f'),
-       ('b879db0c-3bbd-408b-a3b6-98cc7adbf041', 'IN_PROGRESS', 'MEDIUM', '2024-12-05 09:00:00',
-        'a22f9db0-957f-4f95-bfa5-3bca9b03e7b1');
 
 INSERT INTO TasksUsers (task_id, user_id)
 VALUES ('e9f9bd6d-bf44-42ae-823f-c73f7fcf1b3e', 'fef2db0c-2c2e-4f97-b639-7c97b8f8291f'),
@@ -49,14 +44,7 @@ VALUES ('7a4b64d7-77a1-4385-b8f8-b2b1ac592923', 'Create Wireframes', 'Create wir
        ('ab01a4d1-d380-42e9-aab7-b73166d9fc5c', 'Set up Database', 'Set up the database schema for the mobile app.',
         FALSE, 'b879db0c-3bbd-408b-a3b6-98cc7adbf041');
 
-INSERT INTO Comments (content, user_id, task_id, project_id, created_at)
-VALUES ('The homepage design is looking great, but let''s refine the color palette.',
-        'd9a04245-7dff-42c5-a2ab-f8b63ed8c15a', 'e9f9bd6d-bf44-42ae-823f-c73f7fcf1b3e',
-        '15eafbd9-ccfd-42b7-8c88-5c5f558b2b44', '2024-12-02 10:00:00'),
-       ('The API is progressing well, we need to add more endpoints.', 'fef2db0c-2c2e-4f97-b639-7c97b8f8291f',
-        'b879db0c-3bbd-408b-a3b6-98cc7adbf041', 'd3012498-9fe3-47b1-98d4-95f39a66d9ff', '2024-12-06 09:00:00');
-
-INSERT INTO Invitation (project_id, user_id, status, created_at, updated_at)
+INSERT INTO Invitations (project_id, user_id, status, created_at, updated_at)
 VALUES ('15eafbd9-ccfd-42b7-8c88-5c5f558b2b44', 'a22f9db0-957f-4f95-bfa5-3bca9b03e7b1', 'PENDING',
         '2024-12-01 09:00:00', '2024-12-01 09:00:00'),
        ('d3012498-9fe3-47b1-98d4-95f39a66d9ff', 'fef2db0c-2c2e-4f97-b639-7c97b8f8291f', 'ACCEPTED',
