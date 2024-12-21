859a10c543e007f27704715b4db6231790b9e637
Kacper <kacper.pelka03@gmail.com>
2024-12-21 02:52:46 +0100

    Remove unused JUnit test

--- notification_service/src/test/java/org/taskmates/notification_service/NotificationServiceApplicationTests.java
+++ notification_service/src/test/java/org/taskmates/notification_service/NotificationServiceApplicationTests.java
@@ -1,13 +1,7 @@
 package org.taskmates.notification_service;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class NotificationServiceApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
 }
